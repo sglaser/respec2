f669875dc1855b648f21acc5e52207d5e311a9b0
Steve Glaser <sglaser@sglaser.com>
2014-05-30 00:20:33 -0700

    use https: not http:, get unofficial.css from sglaser.github.io

--- js/pcisig/style.js
+++ js/pcisig/style.js
@@ -5,27 +5,27 @@
 //  - specStatus: the short code for the specification's maturity level or type (required)
 
 define(
-    ["core/utils",
-     "text!../../stylesheets/unofficial.css"],
-    function (utils, inlinecss) {
+    ["core/utils"/*,
+     "text!../../stylesheets/unofficial.css"*/],
+    function (utils/*, inlinecss*/) {
         return {
             run:    function (conf, doc, cb, msg) {
                 msg.pub("start", "pcisig/style");
                 if (!conf.specStatus) msg.pub("error", "Configuration 'specStatus' is not set, required for pcisig/style");
                 var statStyle = conf.specStatus;
                 var css = "https://";
-                if (statStyle === "unofficial") {
-                    css += "sglaser.github.io/respec/stylesheets/unofficial";
-                }
-                else if (statStyle === "base") {
-                    css += "sglaser.github.io/respec/stylesheets/base";
-                }
-                else {
-                    css += "sglaser.github.io/respec/stylesheets/pcisig-" + statStyle;
-                }
-                //utils.linkCSS(doc, css);
-                $("<style/>").appendTo($("head", $(doc))).text(inlinecss);
-                console.log("inlinecss.length = " + inlinecss.length);
+//                if (statStyle === "unofficial") {
+                    css += "sglaser.github.io/respec/stylesheets/unofficial.css";
+//                }
+//                else if (statStyle === "base") {
+//                    css += "sglaser.github.io/respec/stylesheets/base.css";
+//                }
+//                else {
+//                    css += "sglaser.github.io/respec/stylesheets/pcisig-" + statStyle + ".css";
+//                }
+                utils.linkCSS(doc, css);
+//                $("<style/>").appendTo($("head", $(doc))).text(inlinecss);
+//                console.log("inlinecss.length = " + inlinecss.length);
 
                 msg.pub("end", "pcisig/style");
                 cb();
