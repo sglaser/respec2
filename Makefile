c3a06f2c53870af25cfddd22ae7a49006a10acb0
Steve Glaser <sglaser@sglaser.com>
2014-06-30 22:19:35 -0700

    build

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 all:
+	node tools/build-w3c-common.js 
+	node tools/example-build.js 
+	node tools/test-build.js 
 	node tools-pcisig/build-pcisig-common.js 
 	node tools-pcisig/example-build.js 
 	node tools-pcisig/test-build.js 
--- tools/tools-pcisig/builder.js
+++ tools/tools-pcisig/builder.js
@@ -1,5 +1,4 @@
 #!/usr/local/bin/node
-/*jshint nonew:true, jquery:true, curly:true, noarg:true, forin:true, noempty:true, eqeqeq:true, strict:true, undef:true, bitwise:true, laxcomma:true, browser:true, devel:true */
 
 var fs   = require("fs")
 ,   pth  = require("path")
@@ -7,14 +6,13 @@ var fs   = require("fs")
 ,   pkg  = JSON.parse(fs.readFileSync(pth.join(__dirname, "../package.json"), "utf-8"))
 ,   version = pkg.version
 ,   versionPCISIG = pkg.versionPCISIG
-//,   versionNVIDIA = pkg.versionNVIDIA
+,   versionNVIDIA = pkg.versionNVIDIA
 // ,   builds = pth.join(__dirname, "../builds")
 // ,   versioned = pth.join(builds, "respec-common-" + versionPCISIG + ".js")
 // ,   versioned = pth.join(builds, "respec-common-" + versionNVIDIA + ".js")
 ;
 
 function gitToURL(url) {
-    "use strict";
     url = url.replace(/^git:/, "https:");
     url = url.replace(/\.git$/, "");
     return url;
@@ -27,7 +25,6 @@ function build (options, cb) {
     // optimisation settings
     // note that the paths/includes below will need to change in when we drop those
     // older dependencies
-    "use strict";
     version = options.version || version;
     var config = {
         baseUrl:    pth.join(__dirname, "../js")
@@ -41,7 +38,7 @@ function build (options, cb) {
             }
         }
     ,   name:       "profile-pcisig-common"
-    //,   name:       "profile-nvidia-common"
+    ,   name:       "profile-nvidia-common"
     ,   include:    "requireLib".split(" ")
     ,   out:        options.out
     ,   inlineText: true
@@ -57,12 +54,12 @@ function build (options, cb) {
                             "/* Documentation: http://w3.org/respec/. */\n" +
                             "/* See original source for licenses: " + gitToURL(pkg.repository.url) + " */\n" +
                             "/* See also PCISIG source: " + gitToURL(pkg.repositoryPCISIG.url) + " */\n" +
-//                            "/* See also NVIDIA source: " + gitToURL(pkg.repositoryNVIDIA.url) + " */\n" +
+                            "/* See also NVIDIA source: " + gitToURL(pkg.repositoryNVIDIA.url) + " */\n" +
                             "respecVersion = '" + version + "';\n" +
                             "respecVersionPCISIG = '" + versionPCISIG + "';\n" +
-//                            "respecVersionNVIDIA = '" + versionNVIDIA + "';\n" +
+                            "respecVersionNVIDIA = '" + versionNVIDIA + "';\n" +
                             fs.readFileSync(config.out, "utf8") + "\nrequire(['profile-pcisig-common']);\n");
-     //                       fs.readFileSync(config.out, "utf8") + "\nrequire(['profile-nvidia-common']);\n");
+                            fs.readFileSync(config.out, "utf8") + "\nrequire(['profile-nvidia-common']);\n");
         }
         catch (e) {
             console.log("ERROR", e);
