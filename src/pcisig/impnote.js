16eee26dbf5786fa6c7952c9fb41d6d647d655c0
Steve Glaser <sglaser@sglaser.com>
2017-09-04 17:09:21 -0700

    Initial cut at impnote.js and impnote.css

---
+++ src/pcisig/impnote.js
@@ -0,0 +1,63 @@
+// Module core/impnote
+// Manages PCISIG Implementation Notes, including marking them up, numbering, inserting the title,
+// and injecting the style sheet.
+// These are elements with class "impnote".
+
+import {pub} from "core/pubsubhub";
+import css from "deps/text!pcisig/css/impnote.css";
+//import { fetch as ghFetch, fetchIndex } from "core/github";
+export const name = "pcisig/impnote";
+
+export function run(conf, doc, cb) {
+  function handleIssues($ins) {
+    $(doc).find("head link").first().before($("<style/>").text(css));
+    $ins.each(function (i, inno) {
+      var $inno = $(inno),
+        isImpNote = $inno.hasClass("impnote"),
+        isFeatureAtRisk = $inno.hasClass("atrisk"),
+        isInline = $inno.css("display") !== "block",
+        report = {
+          inline: isInline,
+          content: $inno.html(),
+        };
+      report.type = "impnote";
+      // wrap
+      if (!isInline) {
+        var $div = $(
+          "<div class='" +
+          report.type +
+          (isFeatureAtRisk ? " atrisk" : "") +
+          "'></div>"
+          ),
+          $tit = $(
+            "<div class='" + report.type + "-title'><span></span></div>"
+          ),
+          text = conf.l10n.implementation_note;
+        report.title = $inno.attr("title");
+        $tit.find("span").text(text);
+        if (report.title) {
+          $tit.append(
+            $(
+              "<span style='text-transform: none'>: " + report.title + "</span>"
+            )
+          );
+          $inno.removeAttr("title");
+        }
+        $tit.addClass("marker");
+        $div.append($tit);
+        $inno.replaceWith($div);
+        var body = $inno.removeClass(report.type);
+        $div.append(body);
+      }
+      pub(report.type, report);
+    });
+  }
+
+  var $ins = $(".impnote");
+  if ($ins.length) {
+    handleIssues($ins);
+    cb();
+  } else {
+    cb();
+  }
+}
