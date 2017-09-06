40abaa06fefa19a8aead3bd6b34149d6430a5356
Steve Glaser <sglaser@sglaser.com>
2017-09-06 01:27:07 -0700

    Initial cut at pcisig style / header / conformance changes. Style and header are still very w3c oriented at this point.

--- src/pcisig/conformance.js
+++ src/pcisig/conformance.js
@@ -1,9 +1,9 @@
-// Module w3c/conformance
+// Module pcisig/conformance
 // Handle the conformance section properly.
 import tmpls from "templates";
 import { pub } from "core/pubsubhub";
 
-export const name = "w3c/conformance";
+export const name = "pcisig/conformance";
 const confoTmpl = tmpls["conformance.html"];
 
 export function run(conf, doc, cb) {
@@ -11,6 +11,6 @@ export function run(conf, doc, cb) {
   if ($confo.length) $confo.prepend(confoTmpl(conf));
   // Added message for legacy compat with Aria specs
   // See https://github.com/w3c/respec/issues/793
-  pub("end", "w3c/conformance");
+  pub("end", "pcisig/conformance");
   cb();
 }
