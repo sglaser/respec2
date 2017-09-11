8f8fd566f64e506d5da8d36b5cb1ac72096a48a7
Steve Glaser <sglaser@sglaser.com>
2017-09-11 03:13:52 -0700

    Make numberByChapter work

--- src/pcisig/fig-tbl-eqn-numbering.js
+++ src/pcisig/fig-tbl-eqn-numbering.js
@@ -11,21 +11,22 @@ export const name = "pcisig/fig-tbl-eqn-numbering";
 
 export function run(conf, doc, cb) {
   if (conf.numberByChapter) {
-    var $secs = $("body > section[data-sectno]", doc);
-    var figNumMap = new Map();
-    var tblNumMap = new Map();
-    var eqnNumMap = new Map();
-    for (var i = 0; i < $secs.length; i++) {
-      var $sec = $($secs[i], doc);
-      var first;
+    let $secs = $("body > section[data-secno]", doc);
+    let figNumMap = new Map();
+    let tblNumMap = new Map();
+    let eqnNumMap = new Map();
+    for (let i = 0; i < $secs.length; i++) {
+      let $sec = $($secs[i], doc);
+      let secno = $sec.attr("data-secno");
+      let first;
 
       // Process Figure Captions, populating figNumMap
       first = 0;
       $("figcaption > span.figno", $sec).each(function () {
-          var $figno_elem = $(this);
-          var figno = parseInt($figno_elem.text(), 10);
+          let $figno_elem = $(this);
+          let figno = parseInt($figno_elem.text(), 10);
           if (first === 0) first = figno;
-          var new_figno = $sec["data-secno"] + "-" + (figno - first + 1);
+          let new_figno = secno + "-" + (figno - first + 1);
           figNumMap.set(figno, new_figno);
           $figno_elem.text(new_figno);
         }
@@ -34,10 +35,10 @@ export function run(conf, doc, cb) {
       // Process Table Captions, populating tblNumMap
       first = 0;
       $("caption > span.tblno", $sec).each(function () {
-          var $tblno_elem = $(this);
-          var tblno = parseInt($tblno_elem.text(), 10);
-          if (first === 0) firstTbl = figno;
-          var new_tblno = $sec["data-secno"] + "-" + (tblno - first + 1);
+          let $tblno_elem = $(this);
+          let tblno = parseInt($tblno_elem.text(), 10);
+          if (first === 0) first = tblno;
+          let new_tblno = secno + "-" + (tblno - first + 1);
           tblNumMap.set(tblno, new_tblno);
           $tblno_elem.text(new_tblno);
         }
@@ -46,10 +47,10 @@ export function run(conf, doc, cb) {
       // Process Eqnure Captions, populating eqnNumMap
       first = 0;
       $("figcaption > span.eqnno", $sec).each(function () {
-          var $eqnno_elem = $(this);
-          var eqnno = parseInt($eqnno_elem.text(), 10);
+          let $eqnno_elem = $(this);
+          let eqnno = parseInt($eqnno_elem.text(), 10);
           if (first === 0) first = eqnno;
-          var new_eqnno = $sec["data-secno"] + "-" + (eqnno - first + 1);
+          let new_eqnno = secno + "-" + (eqnno - first + 1);
           eqnNumMap.set(eqnno, new_eqnno);
           $eqnno_elem.text(new_eqnno);
         }
@@ -58,14 +59,14 @@ export function run(conf, doc, cb) {
 
     // Convert Figure References using figNumMap
     $("a.fig-ref > span.figno", doc).each(function () {
-      var old_num = parseInt($(this).text(), 10);
+      let old_num = parseInt($(this).text(), 10);
       if (figNumMap.has(old_num)) {
         $(this).text(figNumMap.get(old_num));
       }
     });
     // Convert List of Figures using figNumMap
     $("li.tofline > a.tocxref > span.figno", doc).each(function () {
-      var old_num = parseInt($(this).text(), 10);
+      let old_num = parseInt($(this).text(), 10);
       if (figNumMap.has(old_num)) {
         $(this).text(figNumMap.get(old_num));
       }
@@ -73,14 +74,14 @@ export function run(conf, doc, cb) {
 
     // Convert Table References using tblNumMap
     $("a.tbl-ref > span.tblno", doc).each(function () {
-      var old_num = parseInt($(this).text(), 10);
+      let old_num = parseInt($(this).text(), 10);
       if (tblNumMap.has(old_num)) {
         $(this).text(tblNumMap.get(old_num));
       }
     });
     // Convert List of Tables using tblNumMap
     $("li.totline > a.tocxref > span.tblno", doc).each(function () {
-      var old_num = parseInt($(this).text(), 10);
+      let old_num = parseInt($(this).text(), 10);
       if (tblNumMap.has(old_num)) {
         $(this).text(tblNumMap.get(old_num));
       }
@@ -88,14 +89,14 @@ export function run(conf, doc, cb) {
 
     // Convert Equation References using eqnNumMap
     $("a.eqn-ref > span.eqnno", doc).each(function () {
-      var old_num = parseInt($(this).text(), 10);
+      let old_num = parseInt($(this).text(), 10);
       if (eqnNumMap.has(old_num)) {
         $(this).text(eqnNumMap.get(old_num));
       }
     });
     // Convert List of Equations using eqnNumMap
     $("li.toeline > a.tocxref > span.eqnno", doc).each(function () {
-      var old_num = parseInt($(this).text(), 10);
+      let old_num = parseInt($(this).text(), 10);
       if (eqnNumMap.has(old_num)) {
         $(this).text(eqnNumMap.get(old_num));
       }
