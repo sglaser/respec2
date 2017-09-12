aedaaabcabd65e4d7ada05abd030e032e8ceb027
Steve Glaser <sglaser@sglaser.com>
2017-09-11 21:07:27 -0700

    Copy jquery.svg.min.js to js/deps/jquery.svg.min.js (file from the submodule jquery-svg)

--- tools/copydeps.js
+++ tools/copydeps.js
@@ -25,6 +25,7 @@ const srcDesMap = new Map([
   ["./node_modules/text/text.js", "./js/deps/"],
   ["./node_modules/url-search-params/build/url-search-params.js", "./js/deps/"],
   ["./node_modules/webidl2/lib/webidl2.js", "./js/deps/"],
+  ["./submodules/jquery-svg/jquery.svg.min.js", "./js/deps/jquery.svg.min.js"],
 ]);
 
 function makePathResolver(base) {
